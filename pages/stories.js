dee380890eb0e79d7b39da14765a40476df3cf9e
Emilio <emilio.pg95@gmail.com>
2023-04-16 18:01:03 -0600

    🚧 Formatting Stories

--- pages/stories.js
+++ pages/stories.js
@@ -1,24 +1,26 @@
-import view from "../utils/view.js";
+import Story from '../components/Story.js';
+import view from '../utils/view.js';
 
 export default async function Stories(path) {
     const stories = await getStories(path);
     const hasStories = stories.length > 0;
-
+                    
     view.innerHTML = `<div>
-    ${hasStories ? stories.map(story => JSON.stringify(story)) : 'No stories'}
-    </div>`;
+        ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1 })).join('') : 'No stories'}
+    </div>`;  
 }
 
 async function getStories(path) {
-    const isHomeRoute = path === '/'
-    if(isHomeRoute) {
-        path = '/news';
+    const isHomeRoute = path === '/';
+    const isNewRoute = path === '/new';
+    if (isHomeRoute) {
+        path = '/news';  
     } else if (isNewRoute) {
-        path = '/newest';
+        path = '/newest';  
     } 
     const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
     const stories = await response.json();
-    return stories
+    return stories;
 }
 
 
